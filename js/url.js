93d5ed93c1655c861b414cf80a5a108505b21cad
Sadek Ali <sadekali1530@gmail.com>
2023-08-30 22:56:27 +0600

    add the code

--- js/url.js
+++ js/url.js
@@ -1,72 +1,46 @@
-const loodPhone = async (searchItem)=>{
-    const res= await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchItem}`);
-    const data= await res.json();
-    const phones=data.data;
+ 
+ 
+ 
+ const loadphone =async(text)=>{
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${text}`)
+  let data =await res.json();
+  let phones = data.data;
+  // console.log(phones)
+  display(phones);
 
-    displayphones(phones);
+ }
+// for  display the data in ui.....
+  const display = phones =>{
+ let constainnar =document.getElementById('display-data');
 
+ //  ......clear old data for show new data .....
+   constainnar.innerHTML='';
+
+
+   phones.forEach(phone => {
+    //  console.log(phone)
+     const displayCard= document.createElement('div');
+     displayCard.classList='card card-compact  bg-amber-50 shadow-xl'
+     displayCard.innerHTML=`
+     <figure><img src="${phone.image}" alt="Shoes" /></figure>
+         <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+           <p>If a dog chews shoes whose shoes does he choose?</p>
+           <div class="card-actions justify-end">
+             <button class="btn btn-primary">Buy Now</button>
+           </div>
+         </div>
+     `;
+     constainnar.appendChild(displayCard)
+   }); 
+  }
+
+// ....for search data ......
+const search =()=>{
+  const text=document.getElementById("search-fild").value
+  document.getElementById("search-fild").value=''
+  loadphone(text);
 }
 
 
-const displayphones =(phones,)=>{
-      const phoneContannar=document.getElementById('phones-continnar')
-      phoneContannar.innerText=''
-      
-      
-      if(phones.length >10){
-        
-        document.getElementById("but-show-all").classList.remove('hidden')
-      }
-      else{
-        document.getElementById("but-show-all").classList.add('hidden')
-      }
-      if(!isShowall){
-        phones=phones.slice(0,12)
-      }
-     
-    //   if(phones.length > 10)
-    
-
-    phones.forEach(phone => {
-        // console.log(phone)
-        const phonecard=document.createElement('div');
-        phonecard.classList=`card card-compact p-4 bg-amber-50 shadow-xl`;
-        phonecard.innerHTML=`
-        <figure><img src="${phone.image}" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-        
-        `;
-        phoneContannar.appendChild(phonecard);
-    });
-      loading(false);  
-    };
-    const heandalSearch=()=>{
-        const searchItem =document.getElementById("search-fild").value;
-        document.getElementById("search-fild").value=''
-        loading(true);
-        loodPhone(searchItem,);
-    }
-    const heandalSearch2 =()=>{
-      const searchItem2 = document.getElementById('search-fild2').value;
-      document.getElementById('search-fild2').value='';
-      loodPhone(searchItem2)
-    }
-
-    const loading  =(isloading)=>{
-      const spinner =document.getElementById("loading")
-      if(isloading){
-        spinner.classList.remove('hidden')
-      }
-      else{
-        spinner.classList.add('hidden')
-      }
-
-    }
-  
+ loadphone()
